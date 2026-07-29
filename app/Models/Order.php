efa1f322ee0fb0b59ad56836bce19d42f5625487
hager-nasser22 <hager.nasser.32002@gmail.com>
2026-07-29 21:04:45 +0200

    Modify Some Errors In Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -12,7 +12,7 @@ class Order extends Model
     protected $fillable = [
         'user_id','status', 'total_price'
     ];
-    public function users(){
+    public function user(){
         return $this->belongsTo(User::class);
     }
     public function items(){
